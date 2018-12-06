203ab1b4d77a2d914a635c74bbb49ed1a40d5f16
Jan-Christoph Klie <git@mrklie.com>
2018-12-06 14:48:23 +0100

    #32 - Release 0.1.0
    - Fix version path

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ except FileNotFoundError:
 
 # Load the package"s __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, NAME, "__version__.py")) as f:
+with open(os.path.join(here, "cassis", "__version__.py")) as f:
     exec(f.read(), about)
 
 

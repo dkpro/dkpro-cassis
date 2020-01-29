eaf79050dd57a08d8df205f2ccf065c1628fb2b3
Jan-Christoph Klie <git@mrklie.com>
2020-01-29 14:48:10 +0100

    - Add package data to setup.py

--- setup.py
+++ setup.py
@@ -77,39 +77,6 @@ with open(os.path.join(here, "cassis", "__version__.py")) as f:
     exec(f.read(), about)
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")
-
-        sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -121,7 +88,6 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=HOMEPAGE,
-    packages=find_packages(exclude="tests"),
     keywords="uima dkpro cas xmi",
 
     project_urls={
@@ -129,6 +95,9 @@ setup(
         "Documentation": "https://cassis.readthedocs.org/",
         "Source Code": "https://github.com/dkpro/dkpro-cassis",
     },
+
+    packages=find_packages(exclude="tests"),
+    package_data={"cassis": ["resources/*.xml"]},
 
     install_requires=install_requires,
     test_suite="tests",
@@ -153,9 +122,4 @@ setup(
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Text Processing :: Linguistic"
     ],
-
-    # $ setup.py publish support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
 )

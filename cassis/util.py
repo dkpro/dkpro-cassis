ac42006fc29df38f36c5313e1e2cb87ea3c3e0ec
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-09-27 17:36:28 +0200

    No issue. Fix issues with arrays in cas_to_comparable_text - added missing import.

--- cassis/util.py
+++ cassis/util.py
@@ -7,7 +7,7 @@ from typing import Dict, Iterable, Set
 import attr
 
 from cassis import Cas
-from cassis.typesystem import FEATURE_BASE_NAME_SOFA, TYPE_NAME_ANNOTATION, FeatureStructure, Type
+from cassis.typesystem import FEATURE_BASE_NAME_SOFA, TYPE_NAME_ANNOTATION, FeatureStructure, Type, is_array
 
 _EXCLUDED_FEATURES = {FEATURE_BASE_NAME_SOFA}
 _NULL_VALUE = "<NULL>"

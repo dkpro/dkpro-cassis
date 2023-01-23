6943264aea42e19dc849d2f9b238b99fcd1bc72d
Richard Eckart de Castilho <richard.eckart@gmail.com>
2023-01-23 09:44:07 +0100

    #268 - GitHub Actions builds do not run due to missing Python version
    - Also upgrade to checkout v3 since v2 is deprecated

--- setup.py
+++ setup.py
@@ -120,7 +120,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries",
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Text Processing :: Linguistic"
